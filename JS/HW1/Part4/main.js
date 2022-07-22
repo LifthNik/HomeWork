4c3b456f0451f69affc724c37d80c629a99c1593
LifthNik <hikki20088@gmail.com>
2022-07-22 05:20:20 +0300

    HW_JS1

--- JS/HW1/Part4/main.js
+++ JS/HW1/Part4/main.js
@@ -2,7 +2,9 @@ let firstName = prompt('Your First Name?');
 let middleName = prompt('Your Middle Name?');
 let age = prompt('Your Age?');
 
-console.log(firstName + ' ' + middleName + ' ' + age);
+let person = `${firstName} ${middleName} ${age}`;
+// console.log(firstName + ' ' + middleName + ' ' + age); //or the hard way.
+console.log(person);
 
 //optional
 alert(`Hello, ${firstName + ' ' + middleName}. You are ${age} years old!`);
