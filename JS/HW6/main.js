9f0af4f9e43284adb9f888d47938b125e2863f68
LifthNik <hikki20088@gmail.com>
2022-08-17 01:33:20 +0300

    HW_JS8_Main

--- JS/HW6/main.js
+++ JS/HW6/main.js
@@ -98,7 +98,6 @@ console.log(sortNums(nums, 'descending'));
 //     ];
 
 // -- відсортувати його за спаданням за monthDuration
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 
 let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
@@ -110,7 +109,7 @@ let coursesAndDurationArray = [
     ];
 
 let sort = coursesAndDurationArray.sort((course1, course2) => course2.monthDuration - course1.monthDuration);
-//
+
 console.log(sort);
 
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
